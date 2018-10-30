964b5a99601b935092028cb2dec0f709266f0aa3
soloPhone <solophone@aliyun.com>
2018-10-30 11:29:43 +0800

    添加按钮声音

--- assets/Script/GameOver.js
+++ assets/Script/GameOver.js
@@ -16,10 +16,15 @@ cc.Class({
         replayBtn: {
             default: null,
             type: cc.Node
+        },
+        buttonAudio: {
+            default: null,
+            type: cc.AudioClip
         }
     },
     onLoad () {
         this.replayBtn.on(cc.Node.EventType.TOUCH_END, (event) => {
+            cc.audioEngine.play(this.buttonAudio);
             cc.director.loadScene('Game');
         });
         this.init();
